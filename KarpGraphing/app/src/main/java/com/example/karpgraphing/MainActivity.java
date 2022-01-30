94eafa7d5ca8f868382d3f00fbcf41df1413dbbf
Yuval Karp <yuval.karp.yu@gmail.com>
2022-01-30 22:56:49 +0200

    travel between screens + intent dialog

--- KarpGraphing/app/src/main/java/com/example/karpgraphing/MainActivity.java
+++ KarpGraphing/app/src/main/java/com/example/karpgraphing/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         //remove app title
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        //add fuctions button
+        //add functions button
         Button add = findViewById(R.id.add);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,34 +31,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //get function
+        Intent getFunc = getIntent();
+        String func = getFunc.getStringExtra("func");
+
+        //graph settings
         GraphView graph = findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
-        series.appendData(new DataPoint(5,5), true, 2);
-        series.appendData(new DataPoint(10,10), true, 2);
-        //graph.addSeries(series);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(-150);
+        graph.getViewport().setMaxY(150);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(4);
+        graph.getViewport().setMaxX(80);
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setScalableY(true);
+
+        //function params
+//        Expression expression = new Expression(func);
+//        Points points = new Points(expression);
+//        points.generatePoints((-15.0),15.0,2);
+//        points.getSeries().setThickness(8);
+//        graph.addSeries(points.getSeries());
+
 
         String preFix = "x^3";
         Expression exp = new Expression(preFix);
         Points p = new Points(exp);
-        Points p2 = new Points(new Expression("x^2"));
-        p2.generatePoints((-10.0),10.0,2);
         p.generatePoints((-15.0),15.0,2);
 
-        graph.getViewport().setYAxisBoundsManual(true);
-        graph.getViewport().setMinY(-150);
-        graph.getViewport().setMaxY(150);
-
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setMinX(4);
-        graph.getViewport().setMaxX(80);
-
-        // enable scaling and scrolling
-        graph.getViewport().setScalable(true);
-        graph.getViewport().setScalableY(true);
-
-        graph.setTitle(preFix);
         p.getSeries().setThickness(8);
-        p2.getSeries().setColor(android.R.color.holo_red_light);
 
         graph.addSeries(p.getSeries());
 
@@ -67,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //move to function menu
     public void goToAddFunction(){
         //add function window intent
         Intent intent  = new Intent(this,addFunction.class);
--- KarpGraphing/app/src/main/java/com/example/karpgraphing/addFunction.java
+++ KarpGraphing/app/src/main/java/com/example/karpgraphing/addFunction.java
@@ -1,7 +1,11 @@
 package com.example.karpgraphing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class addFunction extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class addFunction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_function);
+
+        //intent
+        Intent addFunc = new Intent(this,MainActivity.class);
+
+        //user input
+        EditText func = findViewById(R.id.func);
+        Button submit = findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getIntent().putExtra("func",func.getText().toString());
+                startActivity(addFunc);
+            }
+        });
+
+
     }
+
+
 }
