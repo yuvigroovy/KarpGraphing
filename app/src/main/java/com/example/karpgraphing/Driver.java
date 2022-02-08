c7e3ec540c051fdce922fba5b4d00d7286a0371e
Yuval <yuval.karp.yu@gmail.com>
2022-02-08 16:46:35 +0200

    started color change

--- app/src/main/java/com/example/karpgraphing/Driver.java
+++ app/src/main/java/com/example/karpgraphing/Driver.java
@@ -1,6 +1,7 @@
 package com.example.karpgraphing;
 
 
+import android.graphics.Color;
 import android.graphics.Paint;
 
 import com.jjoe64.graphview.series.DataPoint;
@@ -11,21 +12,21 @@ import java.util.ArrayList;
 public class Driver {
     private ArrayList<LineGraphSeries<DataPoint>> functions;
     private LineGraphSeries<DataPoint> func;
-    private Paint paint;
+    private Color[] colors;
 
     public Driver(){
         this.functions = new ArrayList<LineGraphSeries<DataPoint>>();
         this.func = new LineGraphSeries<DataPoint>();
-        this.paint = new Paint();
+        //colors = new Color[]{new Color.BLUE,};
+
     }
 
     public LineGraphSeries<DataPoint> insertFunction(String fun){
-        paint.setARGB(1,255,0,0);
         Expression expression = new Expression(fun);
         Points points = new Points(expression);
         points.generatePoints((-50.0),50.0,2);
         points.getSeries().setThickness(8);
-        points.getSeries().setCustomPaint(paint);
+        points.getSeries().setColor(Color.RED);
         functions.add(points.getSeries());
         return points.getSeries();
     }
