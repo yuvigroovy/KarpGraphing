2e1e7ce6e8d9028dcd4f852d38906a588f43cd05
Yuval <yuval.karp.yu@gmail.com>
2022-02-23 21:33:10 +0200

    pow dialog works, button grid works

--- app/src/main/java/com/example/karpgraphing/addFunctionActivity.java
+++ app/src/main/java/com/example/karpgraphing/addFunctionActivity.java
@@ -12,41 +12,72 @@ package com.example.karpgraphing;
 
  ***************************************************************************/
 
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 public class addFunctionActivity extends AppCompatActivity {
 
-    Button dialog;
+    Button pow;
+    String function;
+    Dialog dg;
+    EditText func;
+    EditText a;
+    EditText b;
+    int tag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_function);
 
-        Dialog dg = new Dialog(this);
-        dg.setContentView(R.layout.dialog);
-        TextView txt = dg.findViewById(R.id.tx);
-        txt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dg.dismiss();
-            }
-        });
+        //Button key;
 
-        dialog = findViewById(R.id.diag);
-        dialog.setOnClickListener(new View.OnClickListener() {
+        TableLayout keypad = findViewById(R.id.keypad);
+        for (int i = 0; i < 9; i++) {
+            Button key = keypad.findViewWithTag(i+"");
+            key.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    putNum(Integer.parseInt((String) key.getTag()));
+                }
+            });
+        }
+
+        func = findViewById(R.id.func);
+        Button submit = findViewById(R.id.submit);
+
+        pow = findViewById(R.id.Pow);
+        pow.setText(Html.fromHtml("a<sup><small>b</small></sup>"));
+
+        dg = new Dialog(this);
+        dg.setContentView(R.layout.dialog);
+        dg.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        function = "";
+
+        Button submitPow = dg.findViewById(R.id.submitPow);
+        Button cancel = dg.findViewById(R.id.cancel);
+
+        a = dg.findViewById(R.id.a);
+        b = dg.findViewById(R.id.b);
+
+        TextView txt = dg.findViewById(R.id.Title);
+        txt.setText(Html.fromHtml("a<sup><small>b</small></sup>"));
+
+        pow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dg.show();
+                powOnClick();
             }
         });
 
@@ -54,9 +85,11 @@ public class addFunctionActivity extends AppCompatActivity {
         Intent in = getIntent();
         Intent addFunc = new Intent();
 
+
+
+
         //user input
-        EditText func = findViewById(R.id.func);
-        Button submit = findViewById(R.id.submit);
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,7 +99,50 @@ public class addFunctionActivity extends AppCompatActivity {
             }
         });
 
+        submitPow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                putPow();
+            }
+        });
 
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dg.dismiss();
+            }
+        });
+
+
+
+    }
+
+    public void powOnClick(){
+        function = func.getText().toString();
+        dg.show();
+    }
+
+    public void putPow(){
+        function += a.getText().toString();
+        function += '^';
+        function += b.getText().toString();
+        func.setText(function);
+        dg.dismiss();
+    }
+
+    public void putX(View v){
+        function += 'x';
+        func.setText(function);
+    }
+
+    public void putDecimalPoint(View v){
+        function += '.';
+        func.setText(function);
+    }
+
+    public void putNum(int num){
+        function += num;
+        func.setText(function);
     }
 
 
