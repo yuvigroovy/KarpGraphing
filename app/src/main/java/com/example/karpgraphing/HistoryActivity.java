10d2286962f3a414a4324b8eb5f727128e014015
Yuval <yuval.karp.yu@gmail.com>
2022-04-20 20:11:09 +0300

    added exit button on history window

--- app/src/main/java/com/example/karpgraphing/HistoryActivity.java
+++ app/src/main/java/com/example/karpgraphing/HistoryActivity.java
@@ -1,18 +1,28 @@
 package com.example.karpgraphing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class HistoryActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        //remove app title
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         RecyclerView historyList = findViewById(R.id.HistoryList);
         History historyDb = new History(this);
@@ -23,5 +33,19 @@ public class HistoryActivity extends AppCompatActivity {
 
         HistoryListAdapter adapter = new HistoryListAdapter(list);
         historyList.setAdapter(adapter);
+
+        Button close = findViewById(R.id.exitBtn);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exit();
+            }
+        });
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void exit(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
